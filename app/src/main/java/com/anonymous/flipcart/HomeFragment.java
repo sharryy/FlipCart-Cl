9ff2a253ea0822a44ac8483ad25f3efa9ecafff7
Shehryar Amin <ibneadam388@gmail.com>
2021-01-26 12:43:54 +0500

    Banner with ViewPager

--- app/src/main/java/com/anonymous/flipcart/HomeFragment.java
+++ app/src/main/java/com/anonymous/flipcart/HomeFragment.java
@@ -1,20 +1,34 @@
 package com.anonymous.flipcart;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeFragment extends Fragment {
     private RecyclerView categoryRecyclerView;
     private CategoryAdapter categoryAdapter;
+
+    ///////////BANNER SLIDER
+    private ViewPager bannerSliderViewPager;
+    private List<SliderModel> sliderModelList;
+    private int currentPage = 2;
+    private Timer timer;
+    private final long DELAY_TIME = 3000;
+    private final long PERIOD_TIME = 3000;
+    //////////Banner Slider
 
     public HomeFragment() {
         // Required empty public constructor
@@ -31,6 +45,67 @@ public class HomeFragment extends Fragment {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         linearLayoutManager.setOrientation(RecyclerView.HORIZONTAL);
         categoryRecyclerView.setLayoutManager(linearLayoutManager);
+
+        ///////////BANNER SLIDER
+
+        bannerSliderViewPager = view.findViewById(R.id.banner_slider_view_pager);
+        sliderModelList = new ArrayList<>();
+
+        sliderModelList.add(new SliderModel(R.mipmap.ic_home_24px));
+        sliderModelList.add(new SliderModel(R.mipmap.custom_error_icon));
+
+        sliderModelList.add(new SliderModel(R.mipmap.ic_mail_outline_24px_green));
+        sliderModelList.add(new SliderModel(R.mipmap.ic_mail_outline_24px));
+        sliderModelList.add(new SliderModel(R.mipmap.app_icon));
+        sliderModelList.add(new SliderModel(R.mipmap.ic_launcher));
+        sliderModelList.add(new SliderModel(R.mipmap.cart_black));
+        sliderModelList.add(new SliderModel(R.mipmap.profile_placeholder));
+        sliderModelList.add(new SliderModel(R.mipmap.ic_home_24px));
+        sliderModelList.add(new SliderModel(R.mipmap.custom_error_icon));
+
+        sliderModelList.add(new SliderModel(R.mipmap.ic_mail_outline_24px_green));
+        sliderModelList.add(new SliderModel(R.mipmap.ic_mail_outline_24px));
+
+        SliderAdapter sliderAdapter = new SliderAdapter(sliderModelList);
+        bannerSliderViewPager.setAdapter(sliderAdapter);
+        bannerSliderViewPager.setClipToPadding(false);
+        bannerSliderViewPager.setPageMargin(20);
+
+        ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                currentPage = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (state == ViewPager.SCROLL_STATE_IDLE) {
+                    pageLooper();
+                }
+            }
+        };
+        bannerSliderViewPager.addOnPageChangeListener(onPageChangeListener);
+        startBannerSlideshow();
+
+        bannerSliderViewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                pageLooper();
+                stopBannerSlideShow();
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    startBannerSlideshow();
+                }
+                return false;
+            }
+        });
+
+
+        //////////Banner Slider
 
         List<CategoryModel> categoryModelList = new ArrayList<>();
         categoryModelList.add(new CategoryModel("link", "Home"));
@@ -50,4 +125,40 @@ public class HomeFragment extends Fragment {
 
         return view;
     }
+
+    //////////Banner Slider
+    private void pageLooper() {
+        if (currentPage == sliderModelList.size() - 2) {
+            currentPage = 2;
+            bannerSliderViewPager.setCurrentItem(currentPage, false);
+        }
+
+        if (currentPage == 1) {
+            currentPage = sliderModelList.size() - 3;
+            bannerSliderViewPager.setCurrentItem(currentPage, false);
+        }
+    }
+
+    private void startBannerSlideshow() {
+        Handler handler = new Handler();
+        Runnable update = () -> {
+            if (currentPage >= sliderModelList.size()) {
+                currentPage = 1;
+            }
+            bannerSliderViewPager.setCurrentItem(currentPage++, true);
+        };
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        }, DELAY_TIME, PERIOD_TIME);
+    }
+
+    private void stopBannerSlideShow() {
+        timer.cancel();
+    }
+
+    //////////Banner Slider
 }
---
+++ app/src/main/java/com/anonymous/flipcart/SliderModel.java
@@ -0,0 +1,17 @@
+package com.anonymous.flipcart;
+
+public class SliderModel {
+    private int banner;
+
+    public int getBanner() {
+        return banner;
+    }
+
+    public void setBanner(int banner) {
+        this.banner = banner;
+    }
+
+    public SliderModel(int banner) {
+        this.banner = banner;
+    }
+}
